9105384196b24348e26bfff720b3e61d0c686001
Taras Melnychuk <melnychuktaras@gmail.com>
2013-01-22 17:01:52 +0200

    added scale remove

--- collective/contentfiles2aws/handlers.py
+++ collective/contentfiles2aws/handlers.py
@@ -28,6 +28,10 @@ def before_image_remove(obj, event):
     if hasattr(image_obj, 'uploaded_source_id') and image_obj.uploaded_source_id:
         try:
             as3client.delete(image_obj.uploaded_source_id)
+            field = obj.schema['image']
+            for scale in field.getAvailableSizes(obj):
+                id = field.getScaleName(obj, scale=scale)
+                as3client.delete(id)
         except FileClientRemoveError, e:
             IStatusMessage(obj.REQUEST).addStatusMessage(_(e.message),
                                                          type='error')
