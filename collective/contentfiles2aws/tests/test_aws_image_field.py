caa57ab370e72536fcc932a364380abbe2ecd6e4
Taras Melnychuk <melnychuktaras@gmail.com>
2013-02-12 11:53:28 +0200

    update tests

--- collective/contentfiles2aws/tests/test_aws_image_field.py
+++ collective/contentfiles2aws/tests/test_aws_image_field.py
@@ -1,6 +1,9 @@
 import os
 import unittest2
 
+from OFS.Image import Image
+
+from collective.contentfiles2aws.awsfile import AWSFile
 from collective.contentfiles2aws.testing import \
         AWS_CONTENT_FILES_INTEGRATION_TESTING
 
@@ -21,18 +24,28 @@ class AWSImageFieldTestCase(unittest2.TestCase):
         self.awsimage.update(image=self._get_image())
         self.conf_sheet=self.portal.portal_properties.contentfiles2aws
 
-    def test_do_migrate(self):
+    def test_wrapValue(self):
+        field = self.awsimage.schema['image']
+        image = field._wrapValue(self.awsimage, '')
+        self.assert_(isinstance(image, Image))
+
+        self.assert_(isinstance(field._wrapValue(self.awsimage, image), Image))
+        awsfile = AWSFile('awsfile')
+        self.assert_(isinstance(field._wrapValue(self.awsimage, awsfile),
+                                AWSFile))
+
+    def test_get(self):
+        field = self.awsimage.schema['image']
+        image = field.get(self.awsimage)
+        self.assert_(isinstance(image, Image))
+
         self.conf_sheet._updateProperty('USE_AWS', True)
-        image_field = self.awsimage.schema['image']
-        self.assert_(image_field.migrate(self.awsimage))
-
-        image_field._do_migrate(self.awsimage)
-        self.assert_(not image_field.migrate(self.awsimage))
-
-        for n, s in image_field.getAvailableSizes(self.awsimage).items():
-            storage = image_field.getStorage(self.awsimage)
-            scale = storage.get('image_' + n, self.awsimage)
-            self.assertEqual('AWS Image', scale.meta_type)
+        self.awsimage.REQUEST['image_migrate'] = True
+        image = field.get(self.awsimage)
+        self.assert_(isinstance(image, AWSFile))
+        for n in field.getAvailableSizes(self.awsimage).keys():
+            scale = field.getScale(self.awsimage, scale=n)
+            self.assert_(isinstance(scale, AWSFile))
 
     def test_set(self):
         self.conf_sheet._updateProperty('AWS_BUCKET_NAME', 'contentfiles')
@@ -48,7 +61,31 @@ class AWSImageFieldTestCase(unittest2.TestCase):
         for n, s in image_field.getAvailableSizes(awsimage2).items():
             storage = image_field.getStorage(awsimage2)
             scale = storage.get('image_' + n, awsimage2)
-            self.assertEqual('AWS Image', scale.meta_type)
+            self.assertEqual('AWS File', scale.meta_type)
+
+    def test_createScales(self):
+        self.conf_sheet._updateProperty('USE_AWS', True)
+        field = self.awsimage.schema['image']
+        field.sizes['new_scale'] = (100, 100)
+        field.createScales(self.awsimage)
+        new_scale = field.getScale(self.awsimage, scale='new_scale')
+        self.assert_(isinstance(new_scale, AWSFile))
+
+    def test_tag(self):
+        self.conf_sheet._updateProperty('USE_AWS', True)
+        self.conf_sheet._updateProperty('AWS_BUCKET_NAME', 'contentfiles')
+        field = self.awsimage.schema['image']
+
+        self.assertEqual(field.tag(self.awsimage, scale='thumb'),
+                        ('<img src="http://nohost/plone/awsimage/image_thumb"'
+                         ' alt="" title="" height="16" width="16" />'))
+        self.awsimage.REQUEST['image_migrate'] = True
+        field.get(self.awsimage)
+        tag = field.tag(self.awsimage, scale='thumb')
+        self.assert_(
+            tag.startswith('<img src="http://contentfiles.s3.amazonaws.com/'))
+        self.assert_(tag.endswith(('image_thumb_image.gif" alt="" title=""'
+                                   ' height="" width="" />')))
 
 
 def test_suite():
