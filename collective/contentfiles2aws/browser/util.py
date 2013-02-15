f99f825c6c4a44f855569ba8b84a3a1fd68b9747
Taras Melnychuk <melnychuktaras@gmail.com>
2013-02-15 09:28:48 +0200

    added url generator utility for objects and brains.

---
+++ collective/contentfiles2aws/browser/util.py
@@ -0,0 +1,55 @@
+from zope.component import getUtility
+
+from Products.Five.browser import BrowserView
+
+from collective.contentfiles2aws.awsfile import AWSFile
+from collective.contentfiles2aws.interfaces import IAWSFileClientUtility
+
+
+class AWSUtilView(BrowserView):
+    """ Helper view.  """
+
+    def getFieldValue(self, instance, name):
+        field = instance.schema[name]
+        accessor = field.getAccessor(instance)
+        return accessor()
+
+    def getUrlFromBrain(self, brain, name):
+        if hasattr(brain, 'aws_sources') and brain.aws_sources:
+            sid = brain.aws_sources[name]
+            aws_utility = getUtility(IAWSFileClientUtility)
+            bucket_name = aws_utility.getBucketName()
+            client = aws_utility.getFileClient()
+            url = "http://%s.%s" % (bucket_name, client.connection.server)
+
+            filename_prefix = aws_utility.getAWSFilenamePrefix()
+            if filename_prefix:
+                url = '%s/%s' % (url, filename_prefix)
+
+            url = '%s/%s' % (url, sid)
+        else:
+            url = '%s/%s' % (brain.getURL(), name)
+        return url
+
+    def getUrlFromObject(self, instance, name):
+        value = self.getFieldValue(instance, name)
+        if isinstance(value, AWSFile):
+            url = '%s/%s' % (instance.absolute_url(), name)
+        else:
+            url = value.url()
+        return url
+
+    def get_file_url(self, instance, name='file', brain=True):
+        """ Generates url for file stored in field. """
+
+        return brain and self.getUrlFromBrain(instance, name) or \
+            self.getUrlFromObject(instance, name)
+
+    def get_image_url(self, instance, name='image', scale=None, brain=True):
+        """ Generates url for file stored in field. """
+
+        if scale:
+            name = '%s_%s' % (name, scale)
+
+        return brain and self.getUrlFromBrain(instance, name) or \
+            self.getUrlFromObject(instance, name)
---
+++ collective/contentfiles2aws/catalog.py
@@ -0,0 +1,31 @@
+from plone.indexer import indexer
+
+from Products.Archetypes.interfaces import IBaseObject
+
+from collective.contentfiles2aws.awsfile import AWSFile
+from collective.contentfiles2aws.interfaces import IAWSField
+from collective.contentfiles2aws.interfaces import IAWSImageField
+
+@indexer(IBaseObject)
+def aws_sources(obj):
+    sids = {}
+    if not hasattr(obj, 'schema'):
+        return
+
+    obj_fields = obj.schema.fields()
+    for f in obj_fields:
+        if IAWSField.providedBy(f):
+            accessor= f.getAccessor(obj)
+            field_content = accessor()
+            if not isinstance(field_content, AWSFile):
+                return
+
+            fname = f.getName()
+            sids[fname] = field_content.source_id
+            if IAWSImageField.providedBy(f):
+                for n in f.getAvailableSizes(obj).keys():
+                    scale = f.getScale(obj, scale=n)
+                    scale_name = '%s_%s' % (fname, n)
+                    sids[scale_name] = scale.source_id
+    if sids:
+        return sids
