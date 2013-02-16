fb78728b185d534af99a71028c5f943d44f10f57
Taras Melnychuk <melnychuktaras@gmail.com>
2013-02-16 22:07:28 +0200

    fixed image url generation for scales.

--- collective/contentfiles2aws/browser/util.py
+++ collective/contentfiles2aws/browser/util.py
@@ -9,12 +9,18 @@ from collective.contentfiles2aws.interfaces import IAWSFileClientUtility
 class AWSUtilView(BrowserView):
     """ Helper view.  """
 
-    def getFieldValue(self, instance, name):
-        field = instance.schema[name]
+    def getFieldValue(self, instance, name, scale=None):
+        field = instance.getField(name)
+        if scale:
+            return field.getScale(instance, scale=scale)
+
         accessor = field.getAccessor(instance)
         return accessor()
 
-    def getUrlFromBrain(self, brain, name):
+    def getUrlFromBrain(self, brain, name, scale=None):
+        if scale:
+            name = '%s_%s' % (name, scale)
+
         if hasattr(brain, 'aws_sources') and brain.aws_sources:
             sid = brain.aws_sources[name]
             aws_utility = getUtility(IAWSFileClientUtility)
@@ -31,12 +37,12 @@ class AWSUtilView(BrowserView):
             url = '%s/%s' % (brain.getURL(), name)
         return url
 
-    def getUrlFromObject(self, instance, name):
-        value = self.getFieldValue(instance, name)
+    def getUrlFromObject(self, instance, name, scale=None):
+        value = self.getFieldValue(instance, name, scale=scale)
         if isinstance(value, AWSFile):
             url = value.absolute_url()
         else:
-            url = '%s/%s' % (instance.absolute_url(), name)
+            url = '%s/%s_%s' % (instance.absolute_url(), name, scale)
         return url
 
     def get_file_url(self, instance, name='file', brain=True):
@@ -48,8 +54,5 @@ class AWSUtilView(BrowserView):
     def get_image_url(self, instance, name='image', scale=None, brain=True):
         """ Generates url for file stored in field. """
 
-        if scale:
-            name = '%s_%s' % (name, scale)
-
-        return brain and self.getUrlFromBrain(instance, name) or \
-            self.getUrlFromObject(instance, name)
+        return brain and self.getUrlFromBrain(instance, name, scale=scale) or \
+            self.getUrlFromObject(instance, name, scale=scale)
