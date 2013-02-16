696eceb9aba4133c5e295b0279bc9e08e0ad95da
Taras Melnychuk <melnychuktaras@gmail.com>
2013-02-16 14:40:18 +0200

    made migration script more flexible.

--- collective/contentfiles2aws/browser/content_migration.py
+++ collective/contentfiles2aws/browser/content_migration.py
@@ -2,20 +2,36 @@ import transaction
 from zope.component import getUtility, queryMultiAdapter
 from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
+from Products.Archetypes.ArchetypeTool import getType
 
 from plone.indexer.interfaces import IIndexableObject
 
 from collective.contentfiles2aws.interfaces import IAWSFileClientUtility
-
-MTYPES = ('CHArticle', 'CHNewsArticle', 'CHNewsFile', 'CHNewsImage',
-          'CHPlainPromoPage', 'CHProgramPromoPage', 'CHPromoImage',
-          'CHPromoPage', 'CHSponsorAd', 'CHTopic', 'CHTopicImage')
+from collective.contentfiles2aws.interfaces import IAWSField
 
 
 class ContentMigrationView(BrowserView):
     """ Simple browser view for content migration. """
 
     bsize = 100
+
+    def get_types(self):
+        types_ = []
+        pt = getToolByName(self.context, 'portal_types')
+        for type_ in pt.listTypeInfo():
+            meta_type = type_.Metatype()
+            product = type_.product
+            try:
+                t = getType(meta_type, product)
+            except KeyError:
+                continue
+            if 'schema' in t:
+                for f in t['schema'].fields():
+                    if IAWSField.providedBy(f):
+                        tid = type_.id
+                        if tid not in types_:
+                            types_.append(tid)
+        return types_
 
     def get_bsize(self):
         bsize = self.request.get('bsize', 0)
@@ -39,7 +55,7 @@ class ContentMigrationView(BrowserView):
             for b in pc(**query):
                 obj = b.getObject()
                 for f in obj.schema.fields():
-                    if f.type in ('file', 'image'):
+                    if IAWSField.providedBy(f):
                         name = f.getName()
                         migrate = '%s_migrate' % name
                         obj.REQUEST[migrate] = True
@@ -52,8 +68,6 @@ class ContentMigrationView(BrowserView):
         return results
 
     def __call__(self):
-        """
-        """
         aws_utility = getUtility(IAWSFileClientUtility)
         if not aws_utility.active():
             return "AWSStorage isn't active. Nothing to do!"
@@ -65,7 +79,7 @@ class ContentMigrationView(BrowserView):
         migrated = None
         if content_type:
             if content_type.lower() == 'all':
-                migrated = self._migrate(query, MTYPES)
+                migrated = self._migrate(query, self.get_types())
             else:
                 migrated = self._migrate(query, (content_type,))
 
@@ -73,7 +87,7 @@ class ContentMigrationView(BrowserView):
                 result = '\n'.join(['%s: %d fields migrated' % (k, v)
                                     for k, v in migrated.items()])
         else:
-            for ptype in MTYPES:
+            for ptype in self.get_types():
                 query['portal_type'] = ptype
                 result += "%s: %d\n" % (ptype, len(pc(**query)))
 
